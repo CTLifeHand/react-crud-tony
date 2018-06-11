78e3c0f2f3499bd17576c6d8ed083c8f3b84c94c
coderTonyChan <101992315@qq.com>
2018-06-11 09:26:05 +0800

    增加注释

--- src/store/games/reducer.js
+++ src/store/games/reducer.js
@@ -2,7 +2,7 @@ import * as actionType from './action-type';
 
 export const games = (state = [], action = {}) => {
     switch (action.type) {
-        case actionType.SET_GAMES:
+        case actionType.SET_GAMES: // 获取整个Games
             return action.games;
         case actionType.GAME_DELETED:
             return state.filter(item => item._id !== action.gameId)
@@ -11,12 +11,12 @@ export const games = (state = [], action = {}) => {
                 ...state,
                 action.game
             ]
-        case actionType.GAME_UPDATED:
+        case actionType.GAME_UPDATED: // 编辑
             return state.map(item => {
                 if (item._id === action.game._id) return action.game;
                 return item;
             })
-        case actionType.GAME_FETCHED:
+        case actionType.GAME_FETCHED:// 获取到新的Game
             const index = state.findIndex(item => item._id === action.game._id)
             if (index > -1) {
                 return state.map(item => {
